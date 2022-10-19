c000df534668ef7a50e02461bb1308061195f22d
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-19 07:12:19 -0300

    feat: http methods

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ const express = require("express")
 
 const app = express()
 
+app.get("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2"])
+});
 
-//
-app.get("/", (request, response) => {
-    return response.json({message: "Hello world!"})
-})
+app.post("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3"])
+});
 
-// start application. Especify port from run;
-// localhost:4444
+app.put("/courses/:id", (request, response) => {
+    return response.json(["Curso 5", "Curso 2", "Curso 3"])
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Curso 5", "Curso 2", "Curso 7"])
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso 2", "Curso 7"])
+});
+
+
+
+// start application. Especify port from run; // localhost:4444
+
 app.listen(4444, () => {
     console.log("Listening port 4444")
 })
